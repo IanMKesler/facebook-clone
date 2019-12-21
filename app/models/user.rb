7f973b749c8e4481dbcfa0266d3b469f9a79cd60
Ian Kesler <ianmkesler@gmail.com>
2019-12-20 16:14:11 -0800

    Add first and last name with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true, length: { maximum: 250 }
+  validates :last_name, presence:true, length: { maximum: 250 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
