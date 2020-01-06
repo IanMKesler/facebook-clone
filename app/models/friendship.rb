58602aaa24c2f910e2d1b38307902d074250a731
Ian Kesler <ianmkesler@gmail.com>
2020-01-06 14:32:52 -0800

    Add friendship validations and tests

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,6 +2,17 @@ class Friendship < ApplicationRecord
   belongs_to :requester, class_name: "User" 
   belongs_to :requestee, class_name: "User" 
 
+
+  validate :request
   validates :requester_id, presence: true
   validates :requestee_id, presence: true
+
+  def request
+    begin
+      unless FriendRequest.where(inviter_id: requester_id, invitee_id: requestee_id).exists? || FriendRequest.where(inviter_id: requestee_id, invitee_id: requester_id).exists?
+        errors.add(:requestee_id, 'No matching friend request')
+      end
+    rescue
+    end
+  end
 end
--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,4 +1,29 @@
 require 'test_helper'
 
 class FriendshipTest < ActiveSupport::TestCase
+  def setup
+    @friendship = Friendship.new(requester_id: users(:michael).id,
+                                  requestee_id: users(:sarah).id)
+  end
+
+  test 'should be valid' do
+    assert @friendship.valid?
+  end
+
+  test 'should require both requester and requestee id' do
+    @friendship.requester_id = nil
+    assert_not @friendship.valid?
+    @friendship.requester_id = users(:michael).id
+    @friendship.requestee_id = nil
+    assert_not @friendship.valid?
+    @friendship.requestee_id = users(:sarah).id
+  end
+
+  test 'should not allow friendships without a request' do
+    friendship = Friendship.new(requester_id: users(:michael).id,
+                                requestee_id: users(:sam).id)
+    assert_not friendship.valid?
+    friendship.requester_id, friendship.requestee_id = friendship.requestee_id, friendship.requester_id     
+    assert_not friendship.valid?                           
+  end
 end
